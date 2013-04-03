195990fd490ccb0afa3214a1cc8c655e984896f6
Doc Ritezel and Rob Head <pair+doc+rob@codingzeal.com>
2013-04-03 10:03:31 -0700

    Comply with chef on the whole erb thing

--- cookbooks/figaro/recipes/default.rb
+++ cookbooks/figaro/recipes/default.rb
@@ -1,6 +1,6 @@
 node[:applications].each do |app_name, data|
   template "/data/#{app_name}/shared/config/application.yml" do
-    source "application.yml"
+    source "application.yml.erb"
     owner node[:owner_name]
     group node[:owner_name]
     mode 0655
