93dc84b74ba13d41b8f6f308f5723110ece5d903
Doc Ritezel & Rob Head <pair+doc+rob@codingzeal.com>
2013-04-09 10:05:59 -0700

    Fix for figaro on db instances

--- cookbooks/figaro/recipes/default.rb
+++ cookbooks/figaro/recipes/default.rb
@@ -1,9 +1,11 @@
 node[:applications].each do |app_name, data|
-  template "/data/#{app_name}/shared/config/application.yml" do
-    source "application.yml.erb"
-    owner node[:owner_name]
-    group node[:owner_name]
-    mode 0655
-    backup 0
+  unless ['db_master', 'db_slave'].include?(node[:instance_role])
+    template "/data/#{app_name}/shared/config/application.yml" do
+      source "application.yml.erb"
+      owner node[:owner_name]
+      group node[:owner_name]
+      mode 0655
+      backup 0
+    end
   end
 end
